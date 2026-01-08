e0ce4a28045cdbe3b4149d0d937b1ef887540f81
Koralys Figueroa <koralysfigueroa12@gmail.com>
2026-01-08 16:01:52 -0500

    koralys

--- execute_cmd.c
+++ execute_cmd.c
@@ -1,33 +1,31 @@
 #include "hsh.h"
 
 /**
- * execute_cmd - Execute command using fork and execve
- * @argv: Tokenized arguments
+ * execute_cmd - ejecuta un comando
+ * @argv: array de argumentos
  */
 void execute_cmd(char **argv)
 {
-	pid_t pid;
-	int status;
+    pid_t pid;
+    int status;
 
-	if (!argv || !argv[0])
-		return;
+    if (!argv || !argv[0])
+        return;
 
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		return;
-	}
-	if (pid == 0)
-	{
-		if (execvp(argv[0], argv) == -1)
-		{
-			perror("hsh");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-	}
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        return;
+    }
+    if (pid == 0)
+    {
+        if (execvp(argv[0], argv) == -1)
+        {
+            perror("hsh");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+        waitpid(pid, &status, 0);
 }
--- shell_loop.c
+++ shell_loop.c
@@ -1,26 +1,24 @@
 #include "hsh.h"
 
 /**
- * shell_loop - Reads a line, tokenizes it, and executes command
+ * shell_loop - lee una línea, tokeniza y ejecuta comandos
  */
 void shell_loop(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	char **args;
+    char *line = NULL;
+    size_t bufsize = 0;
+    char **args;
 
-	read = getline(&line, &len, stdin);
-	if (read == -1)
-	{
-		free(line);
-		exit(EXIT_SUCCESS);
-	}
+    if (getline(&line, &bufsize, stdin) == -1)
+    {
+        free(line);
+        exit(EXIT_SUCCESS);  /* Ctrl+D termina la shell */
+    }
 
-	args = tokenize(line);
-	if (args)
-		execute_cmd(args);
+    args = tokenize(line);     /* Separa en tokens */
+    if (args[0] != NULL)
+        execute_cmd(args);     /* Ejecuta el comando */
 
-	free_tokens(args);
-	free(line);
+    free_tokens(args);
+    free(line);
 }
